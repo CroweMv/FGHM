4394a4b64858fe398779e0c5b65ae75de01a9e5b
ramya-itara <ramya@itarait.com>
2021-06-30 15:06:05 +0530

    updated_modules_300621

---
+++ oi_crowe_sale_extend/__manifest__.py
@@ -0,0 +1,17 @@
+
+{
+    'name': 'Sale Order Extend',
+    'version': '14.0.1.0',
+    'category': 'Sales',
+    'summary': 'Addition of fields',
+    'description': """Addition of fields""",
+    'author': 'Oodu Implementers Pvt. Ltd.',
+    'website': "https://www.odooimplementers.com",
+    'depends': ['base', 'sale'],
+    'data': ['data/view.xml'],
+    'demo': [ ],
+    'test': [ ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+  }
