ee714e4964c24ead14316ef7656452ea14c43c0a
ramya-itara <ramya@itarait.com>
2021-07-01 15:32:28 +0530

    010721

---
+++ oi_crowe_sale_extend/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Sale Order Extend',
+    'version': '14.0.1.0',
+    'category': 'Sales',
+    'summary': 'Addition of fields',
+    'description': """Addition of fields""",
+    'author': 'Oodu Implementers Pvt. Ltd.',
+    'website': "https://www.odooimplementers.com",
+    'depends': ['base', 'sale'],
+    'data': ['views/sale_view.xml'],
+    'demo': [ ],
+    'test': [ ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+  }
